3811bf6619ace43440112f702e4779db4e808962
bek <45337347+kruzus@users.noreply.github.com>
2025-10-01 19:23:09 -0400

    changed ui should be good for basic testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import { Button } from '../components/ui/button'
+import { Input } from "@/components/ui/input";
 
+import { CopyButton } from '@/components/ui/shadcn-io/copy-button';
 export default function Home() {
   return (
     <>
-      <Button variant="default">Button</Button>
+
+      <div className="flex flex-col gap-2 w-full">
+        {/* First line: input + button centered */}
+        <div className="flex justify-center w-full">
+          <div className="flex items-center gap-2 w-md">
+            <Input type="email" placeholder="URL to shorten" className="flex-1 w-full" />
+            <CopyButton content="Content to copy" size="md" />
+          </div>
+        </div>
+
+        {/* Second line: full-width element */}
+
+      </div>
+
+
+
     </>
   );
 }
